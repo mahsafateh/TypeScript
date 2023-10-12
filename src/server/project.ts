d7711acde832e60b9d404ad59db39b1f977dfd64
Sheetal Nandi <shkamat@microsoft.com>
2023-10-12 12:10:34 -0700

    Do not update typings for project if type aquisition is disabled

--- src/server/project.ts
+++ src/server/project.ts
@@ -1514,6 +1514,7 @@ export abstract class Project implements LanguageServiceHost, ModuleResolutionHo
         unresolvedImports: SortedReadonlyArray<string>,
         newTypings: string[],
     ) {
+        if (!this.getTypeAcquisition().enable) return;
         this.typingsCacheEntry = {
             compilerOptions,
             typeAcquisition,
